3d2715d4a40b202f540a916bb4485514673f231e
h.boroumand <dasf@gmail.com>
2020-11-09 10:51:07 +0330

    modufy consumer and producer addresses

--- cli/console-consumer.sh
+++ cli/console-consumer.sh
@@ -3,4 +3,4 @@ clear
 
 #./brokers/b0/bin/kafka-console-producer.sh --bootstrap-server 127.0.0.1:9003  --topic test --producer.config ./brokers/b0/config/producer-ssl.properties
 
-./brokers/b0/bin/kafka-console-consumer.sh --bootstrap-server=127.0.0.1:9003 --topic test --consumer.config ./brokers/b0/config/consumer-ssl.properties
+../server/bin/kafka-console-consumer.sh --bootstrap-server=127.0.0.1:9003 --topic test --consumer.config ../server/config/consumer-ssl.properties
