58fb7dc8779d52be9ade96a53d1cc04dbebe2464
h.boroumand <dasf@gmail.com>
2020-11-09 11:37:54 +0330

    editing the path for kafka server logs

--- cli/b1-start.sh
+++ cli/b1-start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 clear
-SERVER_IP=192.168.138.130
 BROKER_ID=1
+SERVER_IP=192.168.138.130
 PORT="9${BROKER_ID}93"
 JMX_PORT="9${BROKER_ID}99"
-export KAFKA_HEAP_OPTS="-Xms128M -Xmx512M -Dkafka.broker0"
+KAFKA_LOGS_DIR="../logs/b${BROKER_ID}"
+
+export LOG_DIR=$KAFKA_LOGS_DIR
+export KAFKA_HEAP_OPTS="-Xms128M -Xmx512M -Dkafka.broker${BROKER_ID}"
+
 sh ../server/bin/kafka-server-start.sh ../server/config/server.properties --override listeners=SSL://$SERVER_IP:9003 --override broker.id=$BROKER_ID --override port=$PORT
