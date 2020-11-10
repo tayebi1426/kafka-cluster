23cd89adcc8f6b1afa50536451d367521d28fc01
h.boroumand <dasf@gmail.com>
2020-11-10 17:02:55 +0330

    ssl edit path

--- cli/console-producer.sh
+++ cli/console-producer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 clear
 
-../server/bin/kafka-console-producer.sh --bootstrap-server 127.0.0.1:9003  --topic test --producer.config ../server/config/producer-ssl.properties
+../server/bin/kafka-console-producer.sh --bootstrap-server 127.0.0.1:9093  --topic test --producer.config ../server/config/producer-ssl.properties
