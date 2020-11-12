dfadee959a856b6935aed51c061cfcdccce70114
h.tayebi <tayebi1426@gmail.com>
2020-11-12 15:49:38 +0330

    ssl-key-generator

--- security-tools/client-ssl-generate.sh
+++ security-tools/client-ssl-generate.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
 
-if [ $1 -lt  ]; then
-	echo "please enter client's name !"
+if [ $# -lt 2 ]; then
+	echo "please, enter client's name & storepass !"
 	exit 1
 fi
 
 set -eu
-CLIENT_CERT_ALIAS="$1"
-KEYSTORE_FILE="$CLIENT_CERT_ALIAS.keystore.jks"
+CLIENT_NAME="$1"
+CLIENT_CERT_ALIAS="$1-ca"
+KEYSTORE_FILE="${CLIENT_NAME}.keystore.jks"
 VALIDITY_IN_DAYS=3650
 WORKING_DIR="wd"
 
-PRIVATE_KEY_FILE="./keystore/root-ca.key"
-ROOT_CERT_FILE="./keystore/root-ca.crt"
+ROOT_KEY_PATH="./root-ca.key"
+ROOT_CRT_PATH="./root-ca.crt"
 
 CSR_FILE="csr-cert"
-CSR_SRL="ca-cert.srl"
+#CSR_SRL="ca-cert.srl"
 SIGNED_CERT_FILE="signed-cert"
 
-ROOT_CERT_ALIAS="CAroot"
+ROOT_CERT_ALIAS="root-ca"
 
-CN=$CLIENT_CERT_ALIAS
+CN=$CLIENT_NAME
 PASS=$2
 
 rm -rf ./$WORKING_DIR
@@ -33,20 +34,19 @@ keytool -genkey -keystore $WORKING_DIR/$KEYSTORE_FILE \
 keytool -certreq -keystore $WORKING_DIR/$KEYSTORE_FILE -alias $CLIENT_CERT_ALIAS \
   -file $WORKING_DIR/$CSR_FILE -keypass $PASS -storepass $PASS
 
-openssl x509 -req -CA $ROOT_CERT_FILE -CAkey $PRIVATE_KEY_FILE \
+openssl x509 -req -CAkey $ROOT_KEY_PATH -CA $ROOT_CRT_PATH \
   -in $WORKING_DIR/$CSR_FILE -out $WORKING_DIR/$SIGNED_CERT_FILE \
   -days $VALIDITY_IN_DAYS -CAcreateserial
 
 keytool -import -noprompt -keystore $WORKING_DIR/$KEYSTORE_FILE -alias $ROOT_CERT_ALIAS  \
-  -file $ROOT_CERT_FILE -keypass $PASS -storepass $PASS
+  -file $ROOT_CRT_PATH -keypass $PASS -storepass $PASS
 
 keytool -import -noprompt -keystore $WORKING_DIR/$KEYSTORE_FILE -alias $CLIENT_CERT_ALIAS  \
   -file $WORKING_DIR/$SIGNED_CERT_FILE -keypass $PASS -storepass $PASS
 
-
 keytool -export -rfc -keystore $WORKING_DIR/$KEYSTORE_FILE -alias $CLIENT_CERT_ALIAS  \
-  -file $WORKING_DIR/"$CLIENT_CERT_ALIAS.crt" -keypass $PASS -storepass $PASS
+  -file $WORKING_DIR/"${CLIENT_CERT_ALIAS}.crt" -keypass $PASS -storepass $PASS
 
 rm $WORKING_DIR/$CSR_FILE
 rm $WORKING_DIR/$SIGNED_CERT_FILE
-rm $WORKING_DIR/$CSR_SRL
+rm ./root-ca.srl
